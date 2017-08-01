3cf75152f93d89462a1c01857398a5f74d407358
Ivan Kozik <ivan@ludios.org>
2017-08-01 09:05:42 +0000

    Install libgl1-mesa-glx:i386 for Steam

--- lib/role.ex
+++ lib/role.ex
@@ -1,7 +1,11 @@
 defmodule RoleRadeon do
 	def role(_tags \\ []) do
 		%{
-			desired_packages: ["xserver-xorg-video-radeon"],
+			desired_packages: [
+				"xserver-xorg-video-radeon",
+				# Steam needs this on Radeon even with STEAM_RUNTIME enabled
+				"libgl1-mesa-glx:i386",
+			],
 		}
 	end
 end
