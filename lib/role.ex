266fa6fa467fc4f52642e3efede8913625731bbb
Ivan Kozik <ivan@ludios.org>
2017-11-24 06:41:35 +0000

    Install firmware-amd-graphics

--- lib/role.ex
+++ lib/role.ex
@@ -2,6 +2,7 @@ defmodule RoleRadeon do
 	def role(_tags \\ []) do
 		%{
 			desired_packages: [
+				"firmware-amd-graphics",
 				"xserver-xorg-video-radeon",
 				# Steam needs this on Radeon even with STEAM_RUNTIME enabled
 				"libgl1-mesa-glx:i386",
