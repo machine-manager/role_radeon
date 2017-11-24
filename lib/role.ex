4a7c190d37b8eaba9c869ed4baf8c4f0707a6ae3
Ivan Kozik <ivan@ludios.org>
2017-11-24 17:39:42 +0000

    Install xorg configuration for my radeon to avoid using the modesetting driver that has triangular tearing

--- lib/role.ex
+++ lib/role.ex
@@ -1,4 +1,9 @@
+alias Converge.Util
+
 defmodule RoleRadeon do
+	require Util
+	import Util, only: [conf_file: 1]
+
 	def role(_tags \\ []) do
 		%{
 			desired_packages: [
@@ -7,6 +12,7 @@ defmodule RoleRadeon do
 				# Steam needs this on Radeon even with STEAM_RUNTIME enabled
 				"libgl1-mesa-glx:i386",
 			],
+			post_install_unit: conf_file("/etc/X11/xorg.conf.d/20-radeon.conf"),
 		}
 	end
 end
