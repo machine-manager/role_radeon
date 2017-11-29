0ca55fb7f756196b8bf3e2e93bd6deea6f0be7ff
Ivan Kozik <ivan@ludios.org>
2017-11-29 02:49:49 +0000

    Create xorg.conf.d because it might not exist in a new debootstrapped Debian

--- lib/role.ex
+++ lib/role.ex
@@ -1,8 +1,8 @@
-alias Converge.Util
+alias Converge.{All, Util}
 
 defmodule RoleRadeon do
 	require Util
-	import Util, only: [conf_file: 1]
+	import Util, only: [conf_file: 1, conf_dir: 1]
 
 	def role(_tags \\ []) do
 		%{
@@ -12,7 +12,11 @@ defmodule RoleRadeon do
 				# Steam needs this on Radeon even with STEAM_RUNTIME enabled
 				"libgl1-mesa-glx:i386",
 			],
-			post_install_unit: conf_file("/etc/X11/xorg.conf.d/20-radeon.conf"),
+			post_install_unit: %All{units: [
+				conf_dir("/etc/X11"),
+				conf_dir("/etc/X11/xorg.conf.d"),
+				conf_file("/etc/X11/xorg.conf.d/20-radeon.conf"),
+			]}
 		}
 	end
 end
